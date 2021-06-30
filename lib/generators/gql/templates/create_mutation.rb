5f17e592e5ca4d39004a404ab3584bb40b251369
Thiago Pradi <thiago@trustedhealth.com>
2021-06-30 15:44:34 -0300

    Update create mutation

--- lib/generators/gql/templates/create_mutation.rb
+++ lib/generators/gql/templates/create_mutation.rb
@@ -1,11 +1,20 @@
 module Mutations
   class <%= prefixed_class_name('Create') %> < Mutations::BaseMutation
+    class Create<%= name %>Input < Types::BaseInputObject
+      description 'Attributes to create <%= name %>'
+
+      # argument :start_date, GraphQL::Types::ISO8601Date, required: true
+    end
+
     field :<%= singular_name %>, Types::<%= name %>Type, null: true
+    field :errors, [Types::ValidationErrorType], null: true
 
-    argument :attributes, Types::Input::<%= name %>Input, required: true
+    argument :input, Create<%= name %>Input, required: true
 
-    def resolve(attributes:)
-      model = <%= name %>.new(attributes.to_h)
+    def resolve(input:)
+      authorize! <%= name %>, to: :create?
+
+      model = <%= name %>.new(input.to_h)
 
       if model.save
         {<%= singular_name %>: model}
