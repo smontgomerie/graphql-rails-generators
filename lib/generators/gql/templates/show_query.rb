02b8363863cba9615cb679e7cad5da66f64276d7
Thiago Pradi <thiago@trustedhealth.com>
2021-06-30 17:43:20 -0300

    more generators

--- lib/generators/gql/templates/show_query.rb
+++ lib/generators/gql/templates/show_query.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 module Resolvers
-  class <%= @model_name %> < Resolvers::BaseResolver
-    type [Types::<%= @model_name %>Type], null: false
-    description "Shows <%= @model_name.downcase.pluralize %>"
+  class <%= name %> < Resolvers::BaseResolver
+    type Types::<%= name %>Type, null: false
+    description "Shows <%= name.downcase.pluralize %>"
 
     argument :id, ID, required: true
 
-
     def resolve(id:)
-      authorized_scope(::<%= @model_name %>.all).find_by!(uuid: id)
+      authorized_scope(::<%= name %>.all).find_by!(uuid: id)
     end
   end
 end
